1ac4513409637524c63bd5889a26541bca02de97
Xiaodi Wang <xiaodi.wang@student.forsbergsskola.se>
2021-02-03 23:17:28 +0100

    Add Drag and drop

--- Assets/Scripts/IMyDrapable.cs
+++
@@ -1,3 +0,0 @@
-public interface IMyDrapable {
-    void OnPointerOver();
-}
--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -2,16 +2,21 @@ using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Wall : MonoBehaviour, IMySelectable ,IPointerClickHandler,IPointerEnterHandler,IPointerExitHandler,IPointerDownHandler,IPointerUpHandler
+public class Wall : MonoBehaviour, IMySelectable ,IPointerClickHandler,IPointerEnterHandler,IPointerExitHandler,IDragHandler,IDropHandler,IDroppable
 {
-    private SpriteRenderer spriteRenderer;
+    private SpriteRenderer block;
     private GameManager gameManager;
     private SpriteRenderer highLight;
+    private bool isDroppable;
 
     private void Start() {
         var tempArr = GetComponentsInChildren<SpriteRenderer>();
-        highLight = tempArr[0].gameObject == this.gameObject ? tempArr[0] : tempArr[1];
-        spriteRenderer = tempArr[0].gameObject == this.gameObject ? tempArr[1] : tempArr[0];
+        foreach(var temp in tempArr) {
+            if(temp.gameObject == this.gameObject) highLight = temp;
+        }
+        foreach(var temp in tempArr) {
+            if(temp.transform.parent == this.transform) block = temp;
+        }
         highLight.enabled = false;
         gameManager = FindObjectOfType<GameManager>();
     }
@@ -22,30 +27,57 @@ public class Wall : MonoBehaviour, IMySelectable ,IPointerClickHandler,IPointerE
     
     void ChangeColor() {
         gameManager.Selected = GetComponent<IMySelectable>();
-        spriteRenderer.color = Color.black;
+        if(block !=null) block.color = Color.black;
     }
     
     public void RemoveSelect() {
-        spriteRenderer.color = Color.gray;
+        if(block !=null) block.color = Color.gray;
     }
     
-    public void OnPointerDown(PointerEventData eventData) {
-        gameManager.dragged = spriteRenderer.gameObject;
-        spriteRenderer.transform.parent = null;
-        var v3 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        v3.z = -5f;
-        spriteRenderer.transform.position = v3;
-    }
+    /*public void OnPointerDown(PointerEventData eventData) {
+        if(block != null) {
+            Debug.Log($"PonterDown:{eventData.pointerCurrentRaycast.gameObject.name}");
+            gameManager.dragged = block.gameObject;
+            block.transform.parent = null;
+            var v3 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            v3.z = -5f;
+            block.transform.position = v3;
+        }
+    }*/
 
     public void OnPointerUp(PointerEventData eventData) {
-        // Debug.Log(eventData.pointerCurrentRaycast.gameObject.name);
+        Debug.Log(eventData.pointerCurrentRaycast.gameObject.name);
     }
 
     public void OnPointerEnter(PointerEventData eventData) {
         highLight.enabled = true;
+        isDroppable = true;
     }
 
     public void OnPointerExit(PointerEventData eventData) {
         highLight.enabled = false;
+        isDroppable = false;
+    }
+
+    public void OnDrag(PointerEventData eventData) {
+        if(block != null) {
+            gameManager.dragged = block.gameObject;
+            block.transform.parent = null;
+            var v3 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            v3.z = -5f;
+            block.transform.position = v3;
+        }
+    }
+
+    public void OnDrop(PointerEventData eventData) {
+        var target = eventData.pointerCurrentRaycast.gameObject;
+        Debug.Log($"Target: {target.name}");
+        var blockTransform = block.transform;
+        blockTransform.parent = target.GetComponent<IDroppable>() != null ? target.transform : this.transform;
+        blockTransform.localPosition = Vector3.zero;
+    }
+
+    public bool IsDroppable() {
+        return isDroppable;
     }
 }
