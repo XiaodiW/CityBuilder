cf146f1e65ce41cfebf24db1fbd9ec6b100d0213
Xiaodi Wang <xiaodi.wang@student.forsbergsskola.se>
2021-02-03 19:50:26 +0100

    Add AutoSaver, Implement wall color changed IPointClick

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Wall : MonoBehaviour
+public class Wall : MonoBehaviour, IPointerClickHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    int index;
+
+    private SpriteRenderer _spriteRenderer;
+ 
+    public void OnPointerClick(PointerEventData eventData)
     {
-        
+        ChangeColor();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        index = 0;
+        _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
+ 
+    void ChangeColor()
+    {
+        if (index == 0)
+        {
+            _spriteRenderer.color = Color.black;
+        }
+        else
+        {
+            _spriteRenderer.color = Color.gray;
+        }
+           
+        index = index == 0 ? 1 : 0;
     }
 }
